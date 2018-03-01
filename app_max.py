fd0a2d371c3356c117cc353194368a3c31483c5a
vsel <vselyukh@gmail.com>
2018-03-01 22:59:37 +0200

    aaaa

--- app_max.py
+++ app_max.py
@@ -22,79 +22,13 @@ def can_get_on_time(
 
 
 # revenue = get_ride_dist*can_deliver_on_time + on_time_bonus*can_get_on_time
-def get_revenue(params, ride, current_time, car_x, car_y):
+def get_revenue(bonus, ride, current_time, car_x, car_y):
     distance_price = ride['distance']*int(can_deliver_on_time(
         ride['lastest_finish'], current_time, car_x, car_y, ride["start"][0], ride["start"][1],
         ride["finish"][0], ride["finish"][1]
     ))
-    on_time_bonus = params.bonus * int(can_get_on_time(
+    on_time_bonus = bonus * int(can_get_on_time(
         car_x, car_y, ride["start"][0], ride["start"][1], ride["earliest_start"]
     ))
     return distance_price + on_time_bonus
 
-
-# In[15]:
-
-
-from collections import namedtuple
-
-Params = namedtuple("Params", ["rows", "columns", "vehicles", "rides", "bonus", "time"])
-
-def main(fname):
-    with open(fname) as file:
-        setup_row = file.readline()
-#         print(setup_row)
-        (row_on_grid,
-        columns_on_grid,
-        vehicles_on_fleet,
-        numbers_of_rides,
-        bonus_for_start_in_time,
-        steps) = [int(item) for item in setup_row.rstrip('\n').split()]
-        #print(r,c,f,n,b,t)
-        ride_id = 0
-        rides = []
-        while ride_id < numbers_of_rides:
-            ride = file.readline()
-            a,b,x,y,s,f = [int(item) for item in ride.rstrip('\n').split()]
-            rides.append({
-                'start': (a,b),
-                'finish':(x,y),
-                'distance': abs(a-x)+abs(b-y),
-                'left_bottom_corner_distance': a+b,
-                'earliest_start':s,
-                'lastest_finish':f
-            })
-            ride_id +=1
-        return rides, Params(
-            row_on_grid, columns_on_grid, vehicles_on_fleet, numbers_of_rides, bonus_for_start_in_time, steps
-        )
-
-
-# In[16]:
-
-
-rides, params = main('/work/hashcode2018/a_example.in')
-
-
-# In[25]:
-
-
-rides
-
-
-# In[35]:
-
-
-from collections import defaultdict
-ride_map = defaultdict(list)
-for ride in rides:
-    ride_map[get_revenue(params, ride, 0, 0, 0)].append(ride)
-
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
---
+++ app_vova_s_b.py
@@ -0,0 +1,71 @@
+
+# coding: utf-8
+
+def find_most_blizkiy(ostavshiesya_list, tazik):
+    if len(ostavshiesya_list)>0:
+        rides_list = {}
+        for id_, ride in enumerate(ostavshiesya_list):
+            rides_list[id_] = abs(tazik[0]-ride['start'][0])+abs(tazik[1]-ride['start'][1])
+        take_ride_id = min(rides_list, key=rides_list.get)
+        tazik[0] = ostavshiesya_list[take_ride_id]['finish'][0]
+        tazik[1] =ostavshiesya_list[take_ride_id]['finish'][1]
+        tazik[2] = rides_list[take_ride_id]
+        tazik[3].append(ostavshiesya_list[take_ride_id]['ride_id'])
+        del ostavshiesya_list[take_ride_id]
+    return ostavshiesya_list
+    
+with open('/Users/Volodymyr/Downloads/hashcode2018/b_should_be_easy.in') as file:
+    setup_row = file.readline()
+    (row_on_grid,
+     columns_on_grid,
+     vehicles_on_fleet,
+     numbers_of_rides,
+     bonus_for_start_in_time,
+     steps) = [int(item) for item in setup_row.rstrip('\n').split()]
+    for_print = {
+        'row_on_grid': row_on_grid,
+        'columns_on_grid': columns_on_grid,
+        'vehicles_on_fleet':vehicles_on_fleet,
+        'numbers_of_rides':numbers_of_rides,
+        'bonus_for_start_in_time':bonus_for_start_in_time,
+        'steps':steps
+    }
+    #print(for_print)
+    ride_id = 0
+    rides = []
+    while ride_id < numbers_of_rides:
+        ride = file.readline()
+        a,b,x,y,s,f = [int(item) for item in ride.rstrip('\n').split()]
+        rides.append({
+            'start': (a,b),
+            'finish':(x,y),
+            'distance': abs(a-x)+abs(b-y),
+            'left_bottom_corner_distance': a+b,
+            'earliest_start':s,
+            'lastest_finish':f,
+            'ride_id':ride_id
+        })
+        ride_id +=1
+    #print(rides)
+#############################
+all_taziks = [[0,0,0,[]] for x in range(vehicles_on_fleet)]
+#print(all_taziks)
+step = 0
+while step<steps:
+    for id_, tazik in enumerate(all_taziks):
+        if tazik[2] == 0:
+            rides= find_most_blizkiy(rides, tazik)
+        else:
+            tazik[2] -= 1 
+    step+=1
+#print(all_taziks)
+list_of_lists = [x[3] for x in all_taziks]
+with open('./hashcode2018/b_should_be_easy.out', 'a') as out_file:
+    vehicle_id = 0
+    list_gen = (x for x in list_of_lists)
+    while vehicle_id < vehicles_on_fleet:
+        result_row = next(list_gen)
+        #print(str(len(result_row))+" "+" ".join(str(x) for x in result_row))
+        vehicle_id +=1
+        out_file.write(str(len(result_row))+" "+" ".join(str(x) for x in result_row)+"\n")
+print('END')
